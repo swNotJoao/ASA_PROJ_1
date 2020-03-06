d952f6f4ec5a2ac39bb449bea2e8b85c03b3dcbb
JCL99 <jclopes99@gmail.com>
2020-03-06 23:39:47 +0000

    add dynamic alloc of dynamic regions

--- src/main.c
+++ src/main.c
@@ -7,8 +7,7 @@ void printGrades();
 typedef struct student student_t;
 typedef struct student{
   int friendNumber;
-  int gradePrediction;
-  int bestGrade;
+  char gradePrediction;
   student_t **friends;
 
 } student_t;
@@ -24,45 +23,47 @@ int main(int argc, char **argv, char **envp) {
 }
 
 void buildGraph(){
-  student_t *student;
   int i, j, grade, src, dest, tmp;
 
-  scanf("%d,%d", &studentNumber, &friendshipNumber);
+  if(scanf("%d,%d", &studentNumber, &friendshipNumber) != 2){};
   graph = (student_t*) malloc(sizeof(student_t) * studentNumber);
 
   for(i = 0; i < studentNumber; i++){
-    scanf("%d", &grade);
+    if(scanf("%d", &grade) != 1){};
     graph[i].friendNumber = 0;
     graph[i].gradePrediction = grade;
-    graph[i].bestGrade = grade;
-    graph[i].friends = (student_t**) calloc(studentNumber, sizeof(student_t*));
+    graph[i].friends = (student_t**) calloc(1024, sizeof(student_t*));
   }
 
   for(i = 0; i < friendshipNumber; i++){
-    scanf("%d %d", &src, &dest);
+    if(scanf("%d %d", &src, &dest) != 2){};
     tmp = graph[src - 1].friendNumber;
+
+    if(tmp % 256 == 0){
+      graph[src - 1].friends = realloc(graph[src - 1].friends, tmp + 1024);
+    }
+
     graph[src - 1].friends[tmp] = &graph[dest - 1];
     graph[src - 1].friendNumber++;
 
-    if(graph[src - 1].bestGrade < graph[dest - 1].bestGrade){
-      graph[src - 1].bestGrade = graph[dest - 1].bestGrade;
+    if(graph[src - 1].gradePrediction < graph[dest - 1].gradePrediction){
+      graph[src - 1].gradePrediction = graph[dest - 1].gradePrediction;
     }
   }
 
   for(i = studentNumber - 1; i >= 0; i--){
     for(j = 0; j < graph[i].friendNumber; j++){
-      if(graph[i].friends[j] -> bestGrade > graph[i].bestGrade){
-        graph[i].bestGrade = graph[i].friends[j] -> bestGrade;
+      if(graph[i].friends[j] -> gradePrediction > graph[i].gradePrediction){
+        graph[i].gradePrediction = graph[i].friends[j] -> gradePrediction;
       }
     }
   }
-
 }
 
 void printGrades(){
   int i;
 
   for(i = 0; i < studentNumber; i++){
-    printf("%d\n", graph[i].bestGrade);
+    printf("%d\n", graph[i].gradePrediction);
   }
 }
