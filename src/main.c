f6831a9779309212d5563795e3adb301b35e1986
JCL99 <jclopes99@gmail.com>
2020-03-06 22:20:25 +0000

    Done, need to test

--- src/main.c
+++ src/main.c
@@ -1,40 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct student student_t;
-
 void buildGraph();
+void printGrades();
 
 typedef struct student{
   int friendNumber;
   int gradePrediction;
+  int bestGrade;
   student_t **friends;
 
 } student_t;
-
 
 int studentNumber, friendshipNumber;
 student_t *graph;
 
 int main(int argc, char **argv, char **envp) {
-
   buildGraph();
+  printGrades();
 
   return 0;
 }
 
 void buildGraph(){
   student_t *student;
-  int i, grade, src, dest, tmp;
+  int i, j, grade, src, dest, tmp;
 
   scanf("%d,%d", &studentNumber, &friendshipNumber);
-  printf("studentNumber = %d\n", studentNumber);
   graph = (student_t*) malloc(sizeof(student_t) * studentNumber);
 
   for(i = 0; i < studentNumber; i++){
     scanf("%d", &grade);
     graph[i].friendNumber = 0;
     graph[i].gradePrediction = grade;
+    graph[i].bestGrade = grade;
     graph[i].friends = (student_t**) calloc(studentNumber, sizeof(student_t*));
   }
 
@@ -43,5 +42,26 @@ void buildGraph(){
     tmp = graph[src - 1].friendNumber;
     graph[src - 1].friends[tmp] = &graph[dest - 1];
     graph[src - 1].friendNumber++;
+
+    if(graph[src - 1].bestGrade < graph[dest - 1].bestGrade){
+      graph[src - 1].bestGrade = graph[dest - 1].bestGrade;
+    }
+  }
+
+  for(i = studentNumber - 1; i >= 0; i--){
+    for(j = 0; j < graph[i].friendNumber; j++){
+      if(graph[i].friends[j] -> bestGrade > graph[i].bestGrade){
+        graph[i].bestGrade = graph[i].friends[j] -> bestGrade;
+      }
+    }
+  }
+
+}
+
+void printGrades(){
+  int i;
+
+  for(i = 0; i < studentNumber; i++){
+    printf("%d\n", graph[i].bestGrade);
   }
 }
