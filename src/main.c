be9345a94984f7f4270fb5550d06c88362a3c2c3
JCL99 <jclopes99@gmail.com>
2020-03-07 21:16:41 +0000

    Clean version

--- src/main.c
+++ src/main.c
@@ -4,15 +4,14 @@
 void buildGraph();
 void printGrades();
 
-typedef struct student student_t;
 typedef struct student{
   unsigned int friendNumber;
   unsigned char gradePrediction;
-  student_t **friends;
-
+  struct student **friends;
 } student_t;
 
 int studentNumber, friendshipNumber;
+int i, j, grade, src, dest, tmp;
 student_t *graph;
 
 int main(int argc, char **argv, char **envp) {
@@ -23,14 +22,11 @@ int main(int argc, char **argv, char **envp) {
 }
 
 void buildGraph(){
-  int i, j, grade, src, dest, tmp;
-
   if(scanf("%d,%d", &studentNumber, &friendshipNumber) != 2){};
-  graph = (student_t*) malloc(sizeof(student_t) * studentNumber);
+  graph = (student_t*) calloc(studentNumber, sizeof(student_t));
 
   for(i = 0; i < studentNumber; i++){
     if(scanf("%d", &grade) != 1){};
-    graph[i].friendNumber = 0;
     graph[i].gradePrediction = grade;
     graph[i].friends = (student_t**) calloc(32, sizeof(student_t*));
   }
@@ -39,31 +35,25 @@ void buildGraph(){
     if(scanf("%d %d", &src, &dest) != 2){};
     tmp = graph[src - 1].friendNumber;
 
-    if(tmp % 32 == 0){
+    if(tmp % 32 == 0)
       graph[src - 1].friends = realloc(graph[src - 1].friends, sizeof(student_t*)*(tmp + 32));
-    }
 
     graph[src - 1].friends[tmp] = &graph[dest - 1];
     graph[src - 1].friendNumber++;
 
-    if(graph[src - 1].gradePrediction < graph[dest - 1].gradePrediction){
+    if(graph[src - 1].gradePrediction < graph[dest - 1].gradePrediction)
       graph[src - 1].gradePrediction = graph[dest - 1].gradePrediction;
-    }
   }
 
   for(i = studentNumber - 1; i >= 0; i--){
     for(j = 0; j < graph[i].friendNumber; j++){
-      if(graph[i].friends[j] -> gradePrediction > graph[i].gradePrediction){
+      if(graph[i].friends[j] -> gradePrediction > graph[i].gradePrediction)
         graph[i].gradePrediction = graph[i].friends[j] -> gradePrediction;
-      }
     }
   }
 }
 
 void printGrades(){
-  int i;
-
-  for(i = 0; i < studentNumber; i++){
+  for(i = 0; i < studentNumber; i++)
     printf("%d\n", graph[i].gradePrediction);
-  }
 }
