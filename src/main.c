6d8bafddc83cbd0087dc403c0c62392c1874c86f
majos95 <majos95@gmail.com>
2020-03-18 16:03:05 +0000

    new dfs cenas

--- src/main.c
+++ src/main.c
@@ -4,68 +4,79 @@
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
 typedef struct student{
-  unsigned int friendNumber;
-  unsigned char gradePrediction;
-  unsigned char visited; /*0 ou 1*/
-  struct student **friends;
+    unsigned int friendNumber;
+    unsigned char gradePrediction;
+    unsigned char visited; /*0 ou 1*/
+    struct student **friends;
 } student_t;
 
-int dfs(student_t *student);
+void dfs_visit(student_t* student, int* count, int* newGrade);
+student_t* find_newMax(student_t *graph);
 
 int studentNumber, friendshipNumber;
-int i, j, k, src, dest, tmp;
+int i, j, k, src, dest, tmp, nVisited  = 0;
 student_t *graph;
 
 int main(int argc, char **argv, char **envp) {
-  if(scanf("%d,%d", &studentNumber, &friendshipNumber) != 2){};
-  graph = (student_t*) calloc(studentNumber, sizeof(student_t));
+    if(scanf("%d,%d", &studentNumber, &friendshipNumber) != 2){};
+    graph = (student_t*) calloc(studentNumber, sizeof(student_t));
 
-  for(i = 0; i < studentNumber; i++){
-    if(scanf("%hhu", &graph[i].gradePrediction) != 1){};
-    graph[i].friends = (student_t**) calloc(32, sizeof(student_t*));
-  }
+    for(i = 0; i < studentNumber; i++){
+        if(scanf("%hhu", &graph[i].gradePrediction) != 1){};
+        graph[i].friends = (student_t**) calloc(32, sizeof(student_t*));
+    }
 
-  for(i = 0; i < friendshipNumber; i++){
-    if(scanf("%d %d", &src, &dest) != 2){};
-    tmp = graph[src - 1].friendNumber;
+    for(i = 0; i < friendshipNumber; i++){
+        if(scanf("%d %d", &src, &dest) != 2){};
+        tmp = graph[dest - 1].friendNumber;
 
-    if(tmp % 32 == 0)
-      graph[src - 1].friends = realloc(graph[src - 1].friends, sizeof(student_t*)*(tmp + 32));
+        if(tmp % 32 == 0)
+            graph[src - 1].friends = realloc(graph[src - 1].friends, sizeof(student_t*)*(tmp + 32));
 
-    graph[src - 1].friends[tmp] = &graph[dest - 1];
-    graph[src - 1].friendNumber++;
+        graph[dest - 1].friends[tmp] = &graph[src - 1];
+        graph[dest - 1].friendNumber++;
+    }
 
-    /*if(graph[src - 1].gradePrediction < graph[dest - 1].gradePrediction)
-      graph[src - 1].gradePrediction = graph[dest - 1].gradePrediction;*/
-  }
+    for (i = 0; i < studentNumber; i++){
+        graph[i].visited = 0;
+    }
 
-  /*for(k = 0; k < 2; k++)
-    for(i = studentNumber - 1; i >= 0; i--)
-      for(j = 0; j < graph[i].friendNumber; j++)
-        if(graph[i].friends[j] -> gradePrediction > graph[i].gradePrediction)
-          graph[i].gradePrediction = graph[i].friends[j] -> gradePrediction;*/
+    while(nVisited < studentNumber){
+        student_t* origin = find_newMax(&graph[0]);
+        int max = origin -> gradePrediction;
+        if (origin -> visited == 0){
+            dfs_visit(origin, &nVisited, &max);
+        }
+    }
 
-  dfs(&graph[0]);
+    for(i = 0; i < studentNumber; i++)
+        printf("%d\n", graph[i].gradePrediction);
 
-  for(i = 0; i < studentNumber; i++)
-    printf("%d\n", graph[i].gradePrediction);
-
-  return 0;
+    return 0;
 }
 
-int dfs(student_t *student){
-  int i, max = student -> gradePrediction;
-
-  if (student -> friends[0] == NULL){
-    student -> gradePrediction = max;
-    return student -> gradePrediction;
-  }
-
-  for (i = 0; i < student -> friendNumber; i++)
-    if (student -> friends[i] -> visited == 0){
-      student -> friends[i] -> visited = 1;
-      max = MAX(dfs(student -> friends[i]), max);
+void dfs_visit(student_t* student, int* count, int* newGrade) {
+    int nEdges = student -> friendNumber;
+    student -> visited = ++(*count);
+    student -> gradePrediction = *newGrade;
+    nVisited++;
+    for (i = 0; i < nEdges; i++){
+        if(student -> friends[i] -> visited == 0){
+            dfs_visit(student -> friends[i], count, newGrade);
+        }
     }
-  student -> gradePrediction = max;
-  return max;
+}
+
+student_t* find_newMax(student_t *graph){
+    unsigned char new_Max = 0;
+    student_t *newMaxStudent = NULL;
+    for(i = 0; i < studentNumber; i++){
+        if(graph[i].visited == 0){
+            if(graph[i].gradePrediction > new_Max) {
+                new_Max = graph[i].gradePrediction;
+                newMaxStudent = &graph[i];
+            }
+        }
+    }
+    return newMaxStudent;
 }
