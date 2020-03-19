3a4354dc05f063f077dc93d5db4f2672746a08ab
swnotjoao <jclopes99@gmail.com>
2020-03-19 18:34:36 +0000

    180 BITCHEZ

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@
 typedef struct student{
   unsigned int friendNumber;
   unsigned char gradePrediction;
-  unsigned char pathsTaken;
+  unsigned char visited;
+  unsigned char maxed;
   struct student **friends;
 } student_t;
 
@@ -15,6 +16,7 @@ student_t *graph, *max_student = NULL;
 void propaga(student_t *student);
 void propaga2(student_t *student);
 void find_newMax();
+void clear();
 
 int main(int argc, char **argv, char **envp) {
   if(scanf("%d,%d", &studentNumber, &friendshipNumber) != 2){};
@@ -44,18 +46,18 @@ int main(int argc, char **argv, char **envp) {
   }
 
   propaga(max_student);
-  max_student -> pathsTaken = 1;
+  max_student -> maxed = 1;
 
-  for(k = 0; k < 2; k++)
+  /*for(k = 0; k < 2; k++)
     for(i = studentNumber - 1; i >= 0; i--)
       for(j = 0; j < graph[i].friendNumber; j++)
         if(graph[i].friends[j] -> gradePrediction < graph[i].gradePrediction)
-          graph[i].friends[j] -> gradePrediction = graph[i].gradePrediction;
+          graph[i].friends[j] -> gradePrediction = graph[i].gradePrediction;*/
 
   for(i = 0; i < studentNumber; i++){
       find_newMax();
       propaga(max_student);
-      max_student -> pathsTaken = 1;
+      clear();
     }
 
   for(i = 0; i < studentNumber; i++)
@@ -66,15 +68,18 @@ int main(int argc, char **argv, char **envp) {
 
 void propaga(student_t *student){
   int i;
+  student -> visited = 1;
 
   /*if (student -> pathsTaken == student -> friendNumber)
     return;*/
 
   for(i = 0; i < student -> friendNumber; i++){
-    if(student -> gradePrediction > student->friends[i] -> gradePrediction){
-      student->friends[i] -> gradePrediction = student -> gradePrediction;
-      /*student -> pathsTaken++;*/
-      propaga(student->friends[i]);
+    if(student -> friends[i] -> visited == 0){
+      if(student -> gradePrediction >= student->friends[i] -> gradePrediction){
+        student->friends[i] -> gradePrediction = student -> gradePrediction;
+        /*student -> pathsTaken++;*/
+        propaga(student->friends[i]);
+      }
     }
   }
   return;
@@ -82,16 +87,25 @@ void propaga(student_t *student){
 
 void find_newMax(){
     for(i = 0; i < studentNumber; i++){
-        if(graph[i].pathsTaken == 0){
+        if(graph[i].maxed == 0){
             max_student = &graph[i];
             break;
         }
     }
 
     for(i = 0; i < studentNumber; i++){
-        if(graph[i].pathsTaken == 0){
+        if(graph[i].maxed == 0){
             if(graph[i].gradePrediction >= max_student->gradePrediction)
               max_student = &graph[i];
         }
     }
+    max_student -> maxed = 1;
+}
+
+void clear(){
+  int i;
+
+  for(i = 0; i < studentNumber; i++){
+    graph[i].visited = 0;
+  }
 }
